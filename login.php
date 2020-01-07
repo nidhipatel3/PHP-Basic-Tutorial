264c80833c5948fb150042a6a4f4617dcb5a691a
nidhipatel3 <jbsnidhipatel14@gmail.com>
2020-01-07 16:07:27 +0530

    login form

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<h1 align="center">Login Form</h1>
+
+<form method="post" align=center action="logincode.php">
+	
+	<input class="input" type="text" name="username" placeholder="username">
+	<br>
+	<br>
+	<input class="input" type="text" name="password" placeholder="password">
+	<br>
+	<br>
+	<input class="btn" type="submit" name="submit" value="login">
+
+</form>
+
+</body>
+</html>
